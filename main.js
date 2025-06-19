f2a4883fae8f4059c744de815a9f56759b5e4e77
osato07 <nakamotosato4@gmail.com>
2025-06-19 22:52:33 +0900

    new commit about google calendar 2

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ function listUpcomingEvents() {
 
     const timeDiffMin = Math.round((start - new Date()) / (1000 * 60));
     const nextEvent = document.getElementById("next-event");
-    nextEvent.textContent = `▶ 次の予定まで ${timeDiffMin}分`;
+    nextEvent.textContent = `▼ 次の予定まで ${timeDiffMin}分`;
   }).catch(error => {
     console.error("❌ APIエラー内容:", error);
   });
